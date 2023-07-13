12ceef8015db93862e39e83e1714d4c7dc5b7f79
SIDHARTHA SANKAR MONDAL <36392277+sidharthamondal@users.noreply.github.com>
2023-07-13 11:09:15 -0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,90 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import os
+
+# Get the full path to the model file
+model_path = os.path.join(os.getcwd(), 'animal_classification_model.h5')
+
+model_path = 'c:\\Users\\SIDHARTH\\Documents\\maths_mini_project\\animal_classification_model.h5'
+# Load the saved model
+model = tf.keras.models.load_model(model_path)
+
+# Define the labels for prediction
+class_labels = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+                'dog', 'frog', 'horse', 'ship', 'truck']
+
+# Configure Streamlit layout
+st.set_page_config(page_title="Animal Classification", layout="wide")
+
+# Custom CSS to enhance the appearance
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #000000;
+            color: #FFFFFF;
+        }
+        .stButton>button {
+            background-color: #F63366;
+            color: #FFFFFF;
+            font-weight: bold;
+            padding: 0.75rem 2rem;
+            border-radius: 0.25rem;
+            border: none;
+            box-shadow: none;
+        }
+        .st-file-uploader {
+            padding: 1rem 0;
+        }
+        .st-file-uploader>div>div:first-child {
+            width: 100%;
+        }
+        .stImage>img {
+            object-fit: contain;
+            max-width: 100%;
+            max-height: 500px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Main layout
+col1, col2 = st.columns([1, 3])
+
+# Left panel - Model capabilities
+with col1:
+    st.image("c:\\Users\\SIDHARTH\\Documents\\maths_mini_project\\images.png", use_column_width=True)
+    st.title("Model Capabilities")
+    st.write("This model can predict the following:")
+    st.write(class_labels)
+
+# Right panel - Image upload and prediction
+with col2:
+    st.title("Animal Classification")
+    st.write("Upload an image and get a prediction!")
+
+    # Image upload section
+    uploaded_image = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+
+    # Perform prediction on the uploaded image
+    if uploaded_image is not None:
+        # Read the image
+        img = image.load_img(uploaded_image, target_size=(32, 32))
+        img_ss = image.load_img(uploaded_image, target_size=(256, 256))
+        
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+
+        # Preprocess the image
+        img_array = img_array / 255.0
+
+        # Make prediction
+        prediction = model.predict(img_array)
+        predicted_label = np.argmax(prediction)
+
+        # Display the predicted label
+        st.write(f"Predicted label: {class_labels[predicted_label]}")
+        st.image(img_ss, caption="Uploaded Image", use_column_width=False)
---
+++ train.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import numpy as np
+import pickle
+
+# Load the CIFAR-10 dataset from the local directory
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+# Load training data
+x_train_all = []
+y_train_all = []
+for i in range(1, 6):
+    data_dict = unpickle(f'cifar-10-batches-py/data_batch_{i}')
+    x_train_batch = data_dict[b'data']
+    y_train_batch = data_dict[b'labels']
+    x_train_all.append(x_train_batch)
+    y_train_all.append(y_train_batch)
+
+x_train = np.concatenate(x_train_all, axis=0)
+y_train = np.concatenate(y_train_all, axis=0)
+
+# Load test data
+test_dict = unpickle('cifar-10-batches-py/test_batch')
+x_test = test_dict[b'data']
+y_test = test_dict[b'labels']
+
+# Reshape input data
+x_train = x_train.reshape(-1, 32, 32, 3)
+x_test = x_test.reshape(-1, 32, 32, 3)
+
+# Normalize pixel values between 0 and 1
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+
+# Convert labels to one-hot encoded vectors
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
+
+# Define the model architecture with Leaky ReLU
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation=tf.nn.leaky_relu, input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation=tf.nn.leaky_relu))
+model.add(layers.Conv2D(128, (3, 3), activation=tf.nn.leaky_relu))
+model.add(layers.Conv2D(128, (3, 3), activation=tf.nn.leaky_relu))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation=tf.nn.leaky_relu))
+model.add(layers.Dense(64, activation=tf.nn.leaky_relu))
+model.add(layers.Dropout(0.25))
+model.add(layers.Dense(10, activation='softmax'))
+# Compile the model
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_test, y_test))
+
+# Evaluate the model
+loss, accuracy = model.evaluate(x_test, y_test)
+print(f'Test Loss: {loss:.4f}')
+print(f'Test Accuracy: {accuracy:.4f}')
+
+model.save('animal_classification_model.h5')
+print("Model saved successfully.")
